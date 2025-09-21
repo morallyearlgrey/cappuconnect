d4d6343daf2f1c376e702f32f34c139e7922c593
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-09-21 15:59:22 -0400

    im illiterate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Cappuconnect",
+  title: "CappuConnect",
   description: "Where connections brew opportunities.",
 };
 
